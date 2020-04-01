e7c46f2257a0badeaefd6b9a5177973c62625b26
Vito <topvetal@gmail.com>
2020-04-01 23:46:06 +0300

    JSON

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import Post from './Post';
+import json from './assets/json';
 import './styles/styles.css';
 const post = new Post('Webpack post title');
 console.log('Post to String', post.toString());
+
+console.log('JSON: ', json.title);
