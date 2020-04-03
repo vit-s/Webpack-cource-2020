4ffab3a23ffa2f73098256a972474ec0ff237a09
Vito <topvetal@gmail.com>
2020-04-03 22:13:44 +0300

    minimizer

--- webpack.config.js
+++ webpack.config.js
@@ -3,9 +3,26 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCssAssetsWebpackPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserWebpackPlugin = require('terser-webpack-plugin');
 
 const isDev = process.env.NODE_ENV === 'development';
 const isProd = !isDev;
+
+const optimization = () => {
+    const config = {
+        splitChunks: {
+            chunks: "all"
+        }
+    };
+    if (isProd) {
+        config.minimizer = [
+            new OptimizeCssAssetsWebpackPlugin(),
+            new TerserWebpackPlugin()
+        ]
+    }
+    return config;
+};
 console.log('IsDev ', isDev);
 module.exports = {
     context: path.resolve(__dirname, 'src'),
@@ -25,11 +42,7 @@ module.exports = {
             '@': path.resolve(__dirname, 'src'),
         }
     },
-    optimization: {
-        splitChunks: {
-            chunks: "all"
-        }
-    },
+    optimization: optimization(),
     devServer: {
         port: 4200,
         hot: isDev
